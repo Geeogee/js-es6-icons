64d1883ae4f9053a308631639a7ed37060e81182
Gioele <gioele.armiento@gmail.com>
2021-04-06 17:45:45 +0200

    Pushed my version of icon exercise

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
-// Milestone 1
-// Partendo dalla seguente struttura dati , mostriamo
-//in pagina tutte le icone disponibili come da layout.
-// Milestone 2
-// Coloriamo le icone per tipo
- //   Milestone 3
-  //   Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+
   function getIconsDb() {
+
     return [
       {
         name: "cat",
@@ -104,109 +99,118 @@
         family: "fas",
       },
     ];
+
   }
+
   function getColors() {
-    return ["blue", "orange", "purple", "rose", "red"];
+    const colors = ["blue", "red", "green", "purple", "yellow"];
+    return colors
   }
-  function init() {
-    // 0. Preleviamo dati
-    const icons = getIconsDb();
-      // console.log(icons);
-    const colors = getColors();
-  //   console.log(colors);
-    const types = getTypes(icons);
-  //   console.log(types);
-    // 1. Coloriamo le icone
-    const coloredIcons = colorIcons(icons, types, colors);
-    console.log(coloredIcons);
-    // 2. Mostriamo gli elementi nella pagina
-    print(coloredIcons);
-    // 3. Aggiungiamo le opizioni al select 
-    addOptions(types);
-    // 4. Mettiamo listener al select con la funzione per filtrare le icone
-    const select = $('#type');
-    select.change(function(event) {
-      const currentType = $(this).val();
-      if (types.includes(currentType)) {
-          // filtriamo l'array in base al tipo
-          const filteredIcons = filterArray(coloredIcons, currentType);
-          console.log(filteredIcons);
-          // mostrare i valori filtrati
-          print(filteredIcons);
-      } else {
-          print(coloredIcons);
+
+  // Milestone 1
+  // Partendo dalla seguente struttura dati , mostriamo
+  //in pagina tutte le icone disponibili come da layout.
+
+  function printIcons(icons) {
+
+    const iconsContainer = document.getElementById("icons-container");
+    iconsContainer.innerHTML = "";
+    icons.forEach(icon => {
+
+      iconsContainer.innerHTML += `<div>
+                                      <i class="${icon.family} ${icon.prefix}${icon.name}" style="color:${icon.color}"></i>
+                                      <div class="title">${icon.name.toUpperCase()}</div>
+                                    </div>`
+    })
+  }
+
+  
+// Milestone 2
+// Coloriamo le icone per tipo
+
+  function getIconType(icons) {
+
+    const types = [];
+
+    icons.forEach(icon => {
+
+      if(!types.includes(icon.type)) {
+        types.push(icon.type)
       }
     });
+
+    return types
   }
-  function print(array) {
-    const container = $(".icons");
-    container.html('');
-    array.forEach((item) => {
-      let color;
-      // if (item.type === 'user') {
-      //     color = 'blue';
-      // } else if (item.type === 'vegetable') {
-      //     color = 'orange';
-      // } else {
-      //     color = 'purple';
-      // }
-      const iconHtml = `
-          <div>
-              <i class="${item.family} ${item.prefix}${item.name}" style="color:${item.color}"></i>
-              <div class="title">${item.name.toUpperCase()}</div>
-          </div> 
-      `;
-      container.append(iconHtml);
-    });
+
+  function colorIcons(icons,colors,iconTypes) {
+
+    icons.map(icon => {
+
+      const iconType = icon.type;
+      const typeIndex = iconTypes.indexOf(iconType);
+      console.log(typeIndex);
+
+      const color = colors[typeIndex];
+      icon.color = color;
+    })
+
+    return icons
   }
-  function getTypes(array) {
-    const types = [];
-    array.forEach((item) => {
-        if(!types.includes(item.type)) {
-            types.push(item.type);
-        }
-    });
-    return types;
-  }
-  function colorIcons(array, types, colors) {
-      const newArray = array.map(item => {
-          const itemClone = {...item};
-          // 1. preleviamo tipologia di icona
-          const iconType = itemClone.type;
-          // 2. preleviamo index di questa tipologia all'interno dell'array
-          const indexType = types.indexOf(iconType);
-          // 3. aggiungiamo color all'itemClone in base all'indice di tipologia
-          const color = colors[indexType];
-          itemClone.color = color;
-          return itemClone;
-      });
-      return newArray;
-      // array.forEach(item => {
-      //     // 1. preleviamo tipologia di icona
-      //     const iconType = item.type;
-      //     // 2. preleviamo index di questa tipologia all'interno dell'array
-      //     const indexType = types.indexOf(iconType);
-      //     // 3. aggiungiamo color all'item in base all'indice di tipologia
-      //     const color = colors[indexType];
-      //     item.color = color;
-      // });
-      // return array;
-  }
+
   function addOptions(types) {
-      const select = $('#type');
-      types.forEach(item => {
-          const optionHtml = `
-              <option value="${item}">${item}</option> 
-          `;
-          select.append(optionHtml);
-      });
+    const select = document.getElementById("type");
+    types.forEach(item => {
+        const option = `
+            <option value="${item}">${item.charAt(0).toUpperCase()}${item.slice(1)}</option> 
+        `;
+        select.innerHTML += option
+    });
+}
+
+function filterArray(icons, typeSelected) {
+
+  const filteredArray = icons.filter(icon => {
+
+    if (icon.type == typeSelected) {
+      return icon
+    }
+  });
+
+  return filteredArray;
+}
+
+  function init() {
+
+    // Collects of all icons
+    const icons = getIconsDb();
+    // Collects all colors
+    const colors = getColors();
+    // Collects all icon types
+    const iconTypes = getIconType(icons);
+    // Add color to each icon
+    colorIcons(icons,colors,iconTypes);
+    // Prints coloured icons
+    printIcons(icons);
+    addOptions(iconTypes);
+
+    const select = document.getElementById("type");
+    select.addEventListener("change", function() {
+
+      const typeSelected = this.value;
+
+      if(iconTypes.includes(typeSelected)) {
+
+        const typeFilterArray = filterArray(icons, typeSelected);
+        printIcons(typeFilterArray);
+      } else {
+
+        printIcons(icons);
+      }
+      
+    })
+    console.log(colors,iconTypes,icons);
+
   }
-  function filterArray(array, key) {
-      const filteredArray = array.filter(item => {
-          if(item.type == key) {
-              return item;
-          }
-      }); 
-      return filteredArray;
-  }
-  $(init);
+
+  document.addEventListener("DOMContentLoaded", init);
+  
